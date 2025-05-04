76ffab40419fc6b8008b359e220e014d6c07b409
kdchang <kdchang.cc@gmail.com>
2025-05-04 19:19:14 +0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# TodoApp use Vue 3 + Vite + Pinia + Vue Router + tailwindcss + daisyui
-
-## Usage
-
-1. `npm install`
--- src/main.js
+++
@@ -1,8 +0,0 @@
-// src/main.js
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-import router from './router'
-import './style.css'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
