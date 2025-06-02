d633210152e868bd55ce24caa927b1ffe655e69c
kdchang <kdchang.cc@gmail.com>
2025-06-02 15:48:01 +0800

    feat: update config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-pinia-todo-app/',
   plugins: [tailwindcss(), vue()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    allowedHosts: [],
+  },
+});
